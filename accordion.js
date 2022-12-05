3d7f10da315d04876f7551d9158d15608823233f
Jaan Nigul <jaannigul@gmail.com>
2022-12-05 23:01:24 +0200

    kommenteerimine

--- accordion.js
+++ accordion.js
@@ -1,4 +1,4 @@
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("accordion"); /*https://www.w3schools.com/howto/howto_js_accordion.asp*/
 var i;
 
 for (i = 0; i < acc.length; i++) {
@@ -12,3 +12,8 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+/*Seda koodi kasutatakse klõpsatava akordioni loomiseks. See loob klõpsatava nupu, mis libiseva animatsiooniga akordioni avab ja sulgeb.
+See algab muutuja "acc" deklareerimisega ja selle määramisega elementidele klassiga "akordion". Seejärel loob see tsükli, mis töötab muutuja "acc" pikkuse ulatuses.
+Loopi sees lisab see "acc" muutuja igale üksusele "sündmuskuulaja". See sündmuste kuulaja käivitatakse nupul klõpsamisel.
+Kui sellel klõpsate, lülitub "aktiivne" klass ja paneeli maxHeight väärtuseks seatakse kas paneeli scrollHeight või null.
+Kui maxHeight on seatud paneeli kerimiskõrgusele, avaneb paneel. Kui see on null, suletakse paneel.*/
